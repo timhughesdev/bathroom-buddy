e8a23ad3dc52efc5090bfa214a8b908c32532fbf
th2n10s4u <th2n10s4u@gmail.com>
2024-07-02 17:10:19 -0700

    fixed signup page

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -4,7 +4,6 @@ import { useNavigate } from 'react-router-dom';
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants';
 import { FormEvent, useState } from 'react';
 
-
 function SignUpPage() {
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
@@ -12,25 +11,23 @@ function SignUpPage() {
   const [loading, setLoading] = useState<boolean>(false);
   const navigate = useNavigate();
 
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
 
-    const [username, setUsername] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
-    const [email, setEmail] = useState<string>('')
-    // const [loading, setLoading] = useState<boolean>(false)
-    const navigate = useNavigate()
-
-    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-
-        try {
-            const res = await backEndApi.post('/api/users/register/', {username, password, email})
-            localStorage.setItem(ACCESS_TOKEN, res.data.access)
-            localStorage.setItem(REFRESH_TOKEN, res.data.refresh)
-            navigate('/login')
-        } 
-        catch (error) {
-            alert(error)
-        }
+    try {
+      const res = await backEndApi.post('/api/users/register/', {
+        username,
+        password,
+        email,
+      });
+      localStorage.setItem(ACCESS_TOKEN, res.data.access);
+      localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+      navigate('/login');
+    } catch (error) {
+      alert('Error creating account. Please try again.');
+    } finally {
+      setLoading(false);
     }
   };
 
