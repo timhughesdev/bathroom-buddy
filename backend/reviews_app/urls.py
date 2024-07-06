953e0cca6fdf760b135175f841ae28d31bd55ef1
Charles Lovelace <cwlovelace@gmail.com>
2024-07-06 17:10:58 -0400

    working on cors

--- backend/reviews_app/urls.py
+++ backend/reviews_app/urls.py
@@ -1,14 +1,15 @@
+# backend/reviews_app/urls.py
 from django.urls import path, register_converter
 from .views import AllReviews, SelectedReview, RestroomReviews
 from .converters import IntConverter
-
 
 # All urls are prefaced by http://localhost:8000/api/v1/reviews/
 
 register_converter(IntConverter, 'int_or_str')
 
 urlpatterns = [
-    path('', AllReviews.as_view(), name='all_reviews'),
-    path('<int_or_str:id>/', SelectedReview.as_view(), name='selected_reviews'),
+    path('all/', AllReviews.as_view(), name='all_reviews'),
+    path('review/<int:id>/', SelectedReview.as_view(), name='selected_review'),
     path('restroom/<int:restroom_id>/', RestroomReviews.as_view(), name='restroom_reviews'),
 ]
+
