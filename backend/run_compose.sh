1eddd2a543936cd5b7b93c436187cb886808d7a3
jordonandersen <jordonandsersen9@gmail.com>
2024-07-11 18:05:25 -0600

    adding scripts

--- backend/run_compose.sh
+++
@@ -1,6 +0,0 @@
-docker-compose up -d --build
-
-# make sure the postgres container is ready, then run migrations
-sleep 5
-docker exec backend-api-1 python /src/manage.py makemigrations 
-docker exec backend-api-1 python /src/manage.py migrate
